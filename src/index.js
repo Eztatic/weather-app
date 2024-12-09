203697428ff5b180aef804eac4a5cc244c0dac6c
Aaron <aakiswa2@gmail.com>
2024-12-09 23:48:24 +0800

    Completed basic weather app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import './style.css';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
 
 const APIKey = '5YQ43Q8F29YCE4N6Z4DVAWAY9';
-let location = 'Tokyo';
+let location = 'Bangkok';
 
 document.querySelector('#search').addEventListener('click', function () {
   location = document.querySelector('#location').value;
   getWeather().then(function (data) {
-    console.log(data);
+    reset();
+
     currentWeather(
       data.address,
       data.currentConditions.precipprob,
@@ -21,8 +24,36 @@ document.querySelector('#search').addEventListener('click', function () {
       data.currentConditions.precip,
       data.currentConditions.uvindex,
     );
+
+    generateHr(data.days[0]);
+
+    generateDays(data.days);
   });
 });
+
+function generateHr(dayData) {
+  const startHour = 6;
+  const step = 3;
+  const totalHours = 6;
+
+  for (let i = 0; i < totalHours; i++) {
+    const hourData = dayData.hours[startHour + i * step];
+    hourlyForecast(hourData.datetime, hourData.icon, hourData.temp);
+  }
+  document.querySelector('.hourly > div > div:last-child').remove();
+}
+
+function generateDays(days) {
+  days.slice(0, 7).forEach((day) => {
+    dailyForecast(
+      day.datetime,
+      day.icon,
+      day.conditions,
+      day.tempmax,
+      day.tempmin,
+    );
+  });
+}
 
 async function getWeather() {
   try {
@@ -62,3 +93,94 @@ const airConditions = (temp, speed, chance, index) => {
   rain.innerText = `${chance === null ? 0 : chance}%`;
   UV.innerText = index;
 };
+
+const hourlyForecast = (hour, img, temp) => {
+  const hours = document.querySelector('.hourly > div');
+  const newHour = document.createElement('div');
+  const time = document.createElement('p');
+  const icon = document.createElement('img');
+  const temperature = document.createElement('p');
+  const separator = document.createElement('div');
+
+  newHour.classList.add('hourly-container');
+  time.classList.add('time');
+  temperature.classList.add('temperature');
+  separator.classList.add('vertical-line');
+
+  icon.setAttribute('src', `./icons/${img}.png`);
+  icon.setAttribute('alt', 'icon');
+
+  dayjs.extend(customParseFormat);
+  time.innerText = dayjs(hour, 'HH:mm:ss').format('h:mm A');
+  temperature.innerText = `${temp}°C`;
+
+  newHour.appendChild(time);
+  newHour.appendChild(icon);
+  newHour.appendChild(temperature);
+  hours.appendChild(newHour);
+  hours.appendChild(separator);
+};
+
+const dailyForecast = (day, img, conditions, maxTemp, minTemp) => {
+  const days = document.querySelector('aside');
+  const dayContainer = document.createElement('div');
+  const date = document.createElement('p');
+  const iconContainer = document.createElement('div');
+  const icon = document.createElement('img');
+  const label = document.createElement('p');
+  const temperature = document.createElement('p');
+  const isToday = dayjs(day).isSame(dayjs(), 'day');
+
+  dayContainer.classList.add('day');
+  date.classList.add('date');
+  label.classList.add('label');
+  temperature.classList.add('temp');
+
+  icon.setAttribute('src', `./icons/${img}.png`);
+  icon.setAttribute('alt', 'icon');
+
+  date.innerText = isToday ? 'Today' : dayjs(day, 'YYYY/MM/DD').format('dddd');
+  label.innerText = conditions;
+  temperature.innerText = `${maxTemp}/${minTemp}`;
+
+  iconContainer.appendChild(icon);
+  iconContainer.appendChild(label);
+  dayContainer.appendChild(date);
+  dayContainer.appendChild(iconContainer);
+  dayContainer.appendChild(temperature);
+  days.appendChild(dayContainer);
+};
+
+function reset() {
+  document.querySelector('.hourly > div').innerText = '';
+  const days = document.querySelector('aside');
+  const label = document.createElement('p');
+  days.innerText = '';
+  label.innerText = '7-Day Forecast';
+  days.appendChild(label);
+}
+
+(function () {
+  getWeather().then(function (data) {
+    reset();
+
+    currentWeather(
+      data.address,
+      data.currentConditions.precipprob,
+      data.currentConditions.temp,
+      data.currentConditions.icon,
+      data.currentConditions.conditions,
+    );
+
+    airConditions(
+      data.currentConditions.feelslike,
+      data.currentConditions.windspeed,
+      data.currentConditions.precip,
+      data.currentConditions.uvindex,
+    );
+
+    generateHr(data.days[0]);
+
+    generateDays(data.days);
+  });
+})();
