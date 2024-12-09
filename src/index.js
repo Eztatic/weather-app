73a9c1a8bc4341956da7ff8bf1e3bc8f264f9ce6
Aaron <aakiswa2@gmail.com>
2024-12-09 12:53:24 +0800

    Displaying data from server

--- src/index.js
+++ src/index.js
@@ -1,24 +1,64 @@
 import './style.css';
 
-// const APIKey = '5YQ43Q8F29YCE4N6Z4DVAWAY9';
-// let location = 'Tokyo';
+const APIKey = '5YQ43Q8F29YCE4N6Z4DVAWAY9';
+let location = 'Tokyo';
 
-// document.querySelector('#search').addEventListener('click', function () {
-//   location = document.querySelector('#location').value;
-//   getWeather().then(function (data) {
-//     console.log(data.days[0].icon);
-//   });
-// });
+document.querySelector('#search').addEventListener('click', function () {
+  location = document.querySelector('#location').value;
+  getWeather().then(function (data) {
+    console.log(data);
+    currentWeather(
+      data.address,
+      data.currentConditions.precipprob,
+      data.currentConditions.temp,
+      data.currentConditions.icon,
+      data.currentConditions.conditions,
+    );
 
-// async function getWeather() {
-//   try {
-//     const weather = await fetch(
-//       `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&elements=datetime%2Cname%2CresolvedAddress%2Ctemp%2Cconditions%2Cdescription%2Cicon&include=days%2Chours%2Cevents&key=5YQ43Q8F29YCE4N6Z4DVAWAY9&contentType=json`,
-//       {mode: 'cors'},
-//     );
-//     const weatherData = await weather.json();
-//     return weatherData;
-//   } catch (error) {
-//     console.error('Error with fetching data:', error);
-//   }
-// }
+    airConditions(
+      data.currentConditions.feelslike,
+      data.currentConditions.windspeed,
+      data.currentConditions.precip,
+      data.currentConditions.uvindex,
+    );
+  });
+});
+
+async function getWeather() {
+  try {
+    const weather = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=5YQ43Q8F29YCE4N6Z4DVAWAY9&contentType=json`,
+      {mode: 'cors'},
+    );
+    const weatherData = await weather.json();
+    return weatherData;
+  } catch (error) {
+    console.error('Error with fetching data:', error);
+  }
+}
+
+const currentWeather = (area, precip, temp, img, cond) => {
+  const location = document.querySelector('.current .location');
+  const rain = document.querySelector('.current .precipitation');
+  const temperature = document.querySelector('.current .temperature');
+  const icon = document.querySelector('.current img');
+  const condition = document.querySelector('.current .icon-label');
+
+  location.innerText = area;
+  rain.innerText = `${precip}%`;
+  temperature.innerText = `${temp}°C`;
+  icon.setAttribute('src', `./icons/${img}.png`);
+  condition.innerText = cond;
+};
+
+const airConditions = (temp, speed, chance, index) => {
+  const temperature = document.querySelector('.real-feel .temperature');
+  const windSpeed = document.querySelector('.wind .speed');
+  const rain = document.querySelector('.rain .chance');
+  const UV = document.querySelector('.uv-index .level');
+
+  temperature.innerText = `${temp}°C`;
+  windSpeed.innerText = `${speed} km/h`;
+  rain.innerText = `${chance === null ? 0 : chance}%`;
+  UV.innerText = index;
+};
